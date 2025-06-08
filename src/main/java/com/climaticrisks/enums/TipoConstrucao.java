5c6bad280886a6c7e81d23c68de26319de3ef742
ftessmann <fernando.tessmann@gmail.com>
2025-06-08 18:32:36 -0300

    feat: update enum

--- src/main/java/com/climaticrisks/enums/TipoConstrucao.java
+++ src/main/java/com/climaticrisks/enums/TipoConstrucao.java
@@ -1,9 +1,31 @@
 package com.climaticrisks.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TipoConstrucao {
-    MADEIRA("madeira"), ALVERNARIA("alvernaria"), MISTA("mista");
+    MADEIRA("madeira"),
+    ALVERNARIA("alvenaria"),
+    MISTA("mista");
 
     private final String valor;
-    TipoConstrucao(String valor) { this.valor = valor; }
-    public String getValor() { return valor; }
+
+    TipoConstrucao(String valor) {
+        this.valor = valor;
+    }
+
+    @JsonValue
+    public String getValor() {
+        return valor;
+    }
+
+    @JsonCreator
+    public static TipoConstrucao fromString(String value) {
+        for (TipoConstrucao tipo : TipoConstrucao.values()) {
+            if (tipo.getValor().equalsIgnoreCase(value)) {
+                return tipo;
+            }
+        }
+        throw new IllegalArgumentException("Tipo de construção inválido: " + value);
+    }
 }
